0880bdd39319e979f9b192ec004d4200c9b636e8
Nikita <mestzatukovky@gmail.com>
2023-03-02 17:27:02 +0700

    Passing buffers to GPU (WIP backup)

--- src/gota_go_fast.wgsl
+++ src/gota_go_fast.wgsl
@@ -1,5 +1,34 @@
 // NOTE: wgpu library example: https://github.com/gfx-rs/wgpu/tree/master/wgpu/examples/hello-compute
 // NOTE: WGSL specs: https://www.w3.org/TR/WGSL/#composite-types
-
+// NOTE: Official tutorial: https://sotrh.github.io/learn-wgpu/beginner/tutorial3-pipeline/#wgsl
 // Arrays: https://www.w3.org/TR/WGSL/#array-types
-// TODO: Is this a thing to create an array of arrays? https://www.w3.org/TR/WGSL/#composite-types
+// NOTE: Is this a thing to create an array of arrays? https://www.w3.org/TR/WGSL/#composite-types
+
+
+@group(0)
+@binding(0)
+// FIXME: Can I just change this to a runtime-sized array?
+// TODO: readonly
+var<storage, read_write> input_keys: array<array<u32, 7>>;
+
+@group(1)
+@binding(0)
+// FIXME: Can I just change this to a runtime-sized array?
+// TODO: readonly
+var<storage, read_write> output_buffer: array<u32, 50>;
+
+fn l2a(key: array<u32, 7>) -> u32{
+    return 420u;
+}
+
+// Thanks __noop__ :)
+fn l2a_chars_count() {
+	// TODO:
+}
+
+@compute
+@workgroup_size(1)
+fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
+	// FIXME: fuuuuuuuuuuuuck....
+    output_buffer[global_id.x] = l2a(input_keys[global_id.x]);
+}
--- src/gpu_bruteforcer.rs
+++ src/gpu_bruteforcer.rs
@@ -2,19 +2,119 @@ use std::{
 	borrow::Cow
 };
 use wgpu::{
-	self
+	self, util::DeviceExt
 };
 
+// WARNING: Key length is hardcoded AND IT'S NOT SYNCED between .wgsl file at the moment. 
+// This is very bad and will be changed later
+const KEY_LENGTH: usize = 7;
 
 // TODO: Check out .wgsl specifications (VERY helpful): https://www.w3.org/TR/WGSL/#numeric-literals
-async fn execute_compute_shader(device: &wgpu::Device, queue: &wgpu::Queue) -> Result<(), wgpu::Error> {
+async fn execute_compute_shader(device: &wgpu::Device, queue: &wgpu::Queue, keys: &[[u32; KEY_LENGTH]]) -> Result<(), wgpu::Error> {
 	// Loading compute shader
 	let shader_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-		label: None,
+		label: Some("Bruteforcer's compute shader"),
 		source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("gota_go_fast.wgsl")))
 	});
 
-	todo!("Figure out how to pass two dimensional array of numbers");
+	// Buffer with array of keys.
+	// Usage allowing the buffer to be:
+    //  - A storage buffer (can be bound within a bind group and thus available to a shader).
+    //  - The destination of a copy.
+    //  - The source of a copy.
+	let storage_keys_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+		label: Some("Keys buffer"),
+		contents: bytemuck::cast_slice(keys),
+		// FIXME: How do I change this to leave readonly access?!
+		usage: wgpu::BufferUsages::STORAGE,
+	});
+
+	// WARNING: THIS IS REALLY BAD. REMOVE LATER AFTER TESTING
+	const EXPECTED_OUTPUT_LENGTH: usize = 50;
+	const OUTPUT_BUFFER_SIZE: u64 = (EXPECTED_OUTPUT_LENGTH * std::mem::size_of::<u32>()) as u64;
+
+	let staging_buffer_thing = device.create_buffer(&wgpu::BufferDescriptor {
+		label: Some("Staging wierd thing"),
+		size: OUTPUT_BUFFER_SIZE,
+		usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
+		mapped_at_creation: true,
+	});
+
+	// Using empty array as init value <-- WARNING: NOTE: THAT'S THE KEY!
+	let bruteforcer_output_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+		label: Some("Output buffer"),
+		contents: bytemuck::cast_slice(&[0_u32; 50]),
+		usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
+	});
+	
+	// A pipeline specifies the operation of a shader.
+    let compute_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+        label: None,
+        layout: None,
+        module: &shader_module,
+        entry_point: "main",
+    });
+
+	// NOTE: Code below prepares buffer data for shader buffer on the GPU with the same binding index
+	// On shader side it looks like this: `layout(set = 0, binding = 0) buffer`
+    let keys_bind_group_layout = compute_pipeline.get_bind_group_layout(0);
+    let keys_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: None,
+        layout: &keys_bind_group_layout,
+        entries: &[
+			wgpu::BindGroupEntry {
+            	binding: 0,
+            	resource: storage_keys_buffer.as_entire_binding(),
+        	}
+		],
+    });
+	let bruteforcer_output_bind_group_layout = compute_pipeline.get_bind_group_layout(1);
+    let bruteforcer_output_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: None,
+        layout: &bruteforcer_output_bind_group_layout,
+        entries: &[
+			wgpu::BindGroupEntry {
+            	binding: 0,
+            	resource: bruteforcer_output_buffer.as_entire_binding(),
+        	}
+		],
+    });
+
+	let mut command_encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+	{
+		let mut compute_pass = command_encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { label: None });
+        compute_pass.set_pipeline(&compute_pipeline);
+        compute_pass.set_bind_group(0, &keys_bind_group, &[]);
+        compute_pass.set_bind_group(1, &bruteforcer_output_bind_group, &[]);
+        compute_pass.insert_debug_marker("l2a bruteforcer");
+		// TODO: scale this later
+        compute_pass.dispatch_workgroups(keys.len() as u32, 1, 1);
+	}
+	// Sets adds copy operation to command encoder.
+    // Will copy data from storage buffer on GPU to staging buffer on CPU.
+    command_encoder.copy_buffer_to_buffer(&bruteforcer_output_buffer, 0, &staging_buffer_thing, 0, OUTPUT_BUFFER_SIZE);
+	
+	queue.submit(Some(command_encoder.finish()));
+	let staging_buffer_thing_slice = staging_buffer_thing.slice(..);
+	// This will map execution result to keys_buffer_slice
+	let (sender, receiver) = futures_intrusive::channel::shared::oneshot_channel();
+    staging_buffer_thing_slice.map_async(wgpu::MapMode::Read, move |v| sender.send(v).unwrap());
+
+	device.poll(wgpu::Maintain::Wait);
+
+	// Awaiting for data from the GPU
+	if let Some(Ok(())) = receiver.receive().await {
+		// 
+		let data = staging_buffer_thing_slice.get_mapped_range();
+		let result: Vec<u32> = bytemuck::cast_slice(&data).to_vec();
+
+		println!("SHADER OUTPUT: {:?}", result);
+		// Freeing buffer from the memory, BUT FIRST it's required to drop all mapped views
+		drop(data);
+		storage_keys_buffer.unmap();
+		bruteforcer_output_buffer.unmap();
+		staging_buffer_thing.unmap();
+	}
 
 	Ok(())
 }
@@ -55,9 +155,10 @@ async fn setup_gpu() -> (wgpu::Device, wgpu::Queue) {
 }
 
 async fn start_bruteforcer() {
-	// let shader_params: Vec<u32> = vec![1, 2, 3, 4];
+	let keys = [[24, 4, 25, 15, 25, 15, 25]];
+
 	let (device, queue) = setup_gpu().await;
-    let gpu_output = execute_compute_shader(&device, &queue).await.unwrap();
+    let gpu_output = execute_compute_shader(&device, &queue, &keys).await.unwrap();
 
     println!("Result: [{:?}]", gpu_output);
     #[cfg(target_arch = "wasm32")]
